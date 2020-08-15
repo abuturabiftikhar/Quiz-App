d405aa1c0d986e8ca49b00e77c188264c1eb6814
Abuturab Iftikhar <63926829+abuturabiftikhar@users.noreply.github.com>
2020-08-15 20:30:17 +0500

    sdsd
    sdsd

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,16 @@
+import { shuffleArray } from './utils';
+import QuestionCard from './components/QuestionCard';
 
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = "easy",
@@ -6,8 +18,17 @@ export enum Difficulty {
   HARD = "hard",
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
-}
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers, 
+      question.correct_answer,
+    ]),
+  }));
+};
